18c4c9d2caca01622bd03d913fe1f4cfc723143d
Daria Hannochenko <soloha7965@gmail.com>
2023-01-27 14:38:06 +0200

    Add AbstractUser  model Cook  in model.py file and add it to settings.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -9,3 +10,11 @@ class DishType(models.Model):
 
     def __str__(self):
         return {self.name}
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ["username"]
+
