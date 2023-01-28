e5c30940e0e4488e34b6a0083abaf3c92b360aca
Daria Hannochenko <soloha7965@gmail.com>
2023-01-28 16:29:34 +0200

    Add view and empty template for main page

--- restaurant_service/urls.py
+++ restaurant_service/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("restaurant.urls", namespace="taxi")),
+    path("admin/", admin.site.urls),
+    path("restaurant/", include("restaurant.urls", namespace="restaurant")),
 ]
