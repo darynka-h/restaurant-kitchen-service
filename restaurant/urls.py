1c44be646ea9a58622a916ff85c61702493c4335
Daria Hannochenko <soloha7965@gmail.com>
2023-01-30 17:42:58 +0200

    Add paths for previous commits in urls.py file

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, DishTypeListView
+from .views import index, DishTypeListView, DishListView, CookListView, CookDetailView, DishDetailView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -8,6 +8,26 @@ urlpatterns = [
         "dish-types/",
         DishTypeListView.as_view(),
         name="dish-type-list"
+    ),
+    path(
+        "dishes/",
+        DishListView.as_view(),
+        name="dish-list",
+    ),
+    path(
+        "dishes/<int:pk>/",
+        DishDetailView.as_view(),
+        name="dish-detail",
+    ),
+    path(
+        "cooks/",
+        CookListView.as_view(),
+        name="cooks-list",
+    ),
+    path(
+        "cooks/<int:pk>/",
+        CookDetailView.as_view(),
+        name="cook-detail"
     ),
 ]
 
