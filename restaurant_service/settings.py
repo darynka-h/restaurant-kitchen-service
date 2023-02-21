206b097f1076eb9e73c79c0e96d21a7371fb1492
Daria Hannochenko <soloha7965@gmail.com>
2023-02-21 17:42:04 +0200

    Add "restaurant-managment-service.onrender.com" to ALLOWED_HOSTS

--- restaurant_service/settings.py
+++ restaurant_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-managment-service.onrender.com"]
 
 
 # Application definition
